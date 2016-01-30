dae2cf226ff351a060b894301e87166db9865cb0
Nick Prühs <dev@npruehs.de>
2016-01-30 02:16:05 +0100

    ADDED: Flow - Winning the game after leaving with all objectives complete.

---
+++ Unity/Rituals/Assets/Game/Scripts/Flow/Components/ExitComponent.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExitComponent.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Rituals.Flow.Components
+{
+    using UnityEngine;
+
+    public class ExitComponent : MonoBehaviour
+    {
+    }
+}
---
+++ Unity/Rituals/Assets/Game/Scripts/Flow/Systems/VictorySystem.cs
@@ -0,0 +1,74 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="VictorySystem.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Rituals.Flow.Systems
+{
+    using Rituals.Core;
+    using Rituals.Flow.Components;
+    using Rituals.Objectives.Events;
+    using Rituals.Physics.Events;
+
+    using UnityEngine;
+
+    public class VictorySystem : RitualsBehaviour
+    {
+        #region Fields
+
+        public Collider PlayerInteractionCollider;
+
+        private bool mayLeave;
+
+        #endregion
+
+        #region Methods
+
+        protected override void AddListeners()
+        {
+            base.AddListeners();
+
+            this.EventManager.CollisionEntered += this.OnCollisionEntered;
+            this.EventManager.ObjectiveStateChanged += this.OnObjectiveStateChanged;
+        }
+
+        protected override void RemoveListeners()
+        {
+            base.RemoveListeners();
+
+            this.EventManager.CollisionEntered -= this.OnCollisionEntered;
+            this.EventManager.ObjectiveStateChanged -= this.OnObjectiveStateChanged;
+        }
+
+        private void OnCollisionEntered(object sender, CollisionEventArgs args)
+        {
+            if (!this.mayLeave)
+            {
+                return;
+            }
+
+            if (args.First != this.PlayerInteractionCollider)
+            {
+                return;
+            }
+
+            var exit = args.Second.GetComponentInParent<ExitComponent>();
+
+            if (exit == null)
+            {
+                return;
+            }
+
+            // Victory!
+            Application.LoadLevel("GameScene");
+        }
+
+        private void OnObjectiveStateChanged(object sender, ObjectiveStateChangedEventArgs args)
+        {
+            this.mayLeave = args.CompletedObjectives == args.TotalObjectives;
+        }
+
+        #endregion
+    }
+}
