f68708ae0f9cda1a6a154df90ae7e69f74aa71cb
Nick Prühs <dev@npruehs.de>
2016-01-30 22:15:33 +0100

    ADDED: Flow - Defeat if pressure exceeds 100%.

---
+++ Unity/Rituals/Assets/Game/Scripts/Flow/Systems/DefeatSystem.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DefeatSystem.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Rituals.Flow.Systems
+{
+    using Rituals.Core;
+    using Rituals.Pressure.Events;
+
+    using UnityEngine;
+
+    public class DefeatSystem : RitualsBehaviour
+    {
+        #region Methods
+
+        protected override void AddListeners()
+        {
+            base.AddListeners();
+
+            this.EventManager.PressureChanged += this.OnPressureChanged;
+        }
+
+        protected override void RemoveListeners()
+        {
+            base.RemoveListeners();
+
+            this.EventManager.PressureChanged -= this.OnPressureChanged;
+        }
+
+        private void OnPressureChanged(object sender, PressureChangedEventArgs args)
+        {
+            if (args.Pressure >= 1)
+            {
+                // Defeat!
+                Application.LoadLevel("MainMenu");
+            }
+        }
+
+        #endregion
+    }
+}
