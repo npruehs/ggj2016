09a673e5bbc5c70a4361c95ddd1c60c351d1ddcd
Nick Prühs <dev@npruehs.de>
2016-01-30 03:05:44 +0100

    ADDED: Menu - Main menu.

---
+++ Unity/Rituals/Assets/Game/Scripts/Menu/MainMenu/LoadSceneOnClick.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LoadSceneOnClick.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Rituals.Menu.MainMenu
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class LoadSceneOnClick : MonoBehaviour
+    {
+        #region Fields
+
+        public Button Button;
+
+        public string Scene;
+
+        #endregion
+
+        #region Methods
+
+        private void OnButtonClicked()
+        {
+            Application.LoadLevel(this.Scene);
+        }
+
+        private void OnDisable()
+        {
+            if (this.Button != null)
+            {
+                this.Button.onClick.RemoveListener(this.OnButtonClicked);
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (this.Button != null)
+            {
+                this.Button.onClick.AddListener(this.OnButtonClicked);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Unity/Rituals/Assets/Game/Scripts/Menu/MainMenu/QuitOnClick.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="QuitOnClick.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Rituals.Menu.MainMenu
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class QuitOnClick : MonoBehaviour
+    {
+        #region Fields
+
+        public Button Button;
+
+        #endregion
+
+        #region Methods
+
+        private void OnButtonClicked()
+        {
+            Application.Quit();
+        }
+
+        private void OnDisable()
+        {
+            if (this.Button != null)
+            {
+                this.Button.onClick.RemoveListener(this.OnButtonClicked);
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (this.Button != null)
+            {
+                this.Button.onClick.AddListener(this.OnButtonClicked);
+            }
+        }
+
+        #endregion
+    }
+}
