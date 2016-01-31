b2d67b1043f6c382aed451ee663b0ae8ffebfa76
Nick Prühs <dev@npruehs.de>
2016-01-31 12:29:01 +0100

    FIXED: UI - Distance indicator arrow points in the right direction.

--- Unity/Rituals/Assets/Game/Scripts/Objectives/Systems/DistanceIndicatorUISystem.cs
+++ Unity/Rituals/Assets/Game/Scripts/Objectives/Systems/DistanceIndicatorUISystem.cs
@@ -7,7 +7,9 @@
 namespace Rituals.Objectives.Systems
 {
     using Rituals.Core;
+    using Rituals.Interaction.Components;
     using Rituals.Objectives.Events;
+    using Rituals.Physics.Components;
 
     using UnityEngine;
     using UnityEngine.UI;
@@ -21,6 +23,22 @@ namespace Rituals.Objectives.Systems
         public Text Text;
 
         private GameObject currentObjective;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///   Returns the angle between the specified vectors in radians.
+        /// </summary>
+        /// <param name="v">First vector.</param>
+        /// <param name="w">Second vector.</param>
+        /// <returns>Angle between the specified vectors in radians.</returns>
+        public static float AngleBetween(Vector2 v, Vector2 w)
+        {
+            // http://www.euclideanspace.com/maths/algebra/vectors/angleBetween/index.htm
+            return Mathf.Atan2(w.y, w.x) - Mathf.Atan2(v.y, v.x);
+        }
 
         #endregion
 
@@ -54,15 +72,20 @@ namespace Rituals.Objectives.Systems
                 return;
             }
 
+            var interactableComponent = this.currentObjective.GetComponentInChildren<InteractableComponent>();
+            var colliderComponent = interactableComponent.GetComponentInChildren<ColliderComponent>();
+            var interactionCollider = colliderComponent.GetComponent<Collider>();
+            var objectivePosition = interactionCollider.bounds.center;
+
             // Get distance to objective.
-            var distanceVector = this.Player.transform.position - this.currentObjective.transform.position;
+            var distanceVector = objectivePosition - this.Player.transform.position;
             var distance = distanceVector.magnitude;
 
             // Get angle between direction to objective and look direction.
-            var direction = new Vector2(distanceVector.x, distanceVector.z);
-            var lookDirection = new Vector2(this.Player.transform.forward.x, this.Player.transform.forward.z);
+            var direction = new Vector2(distanceVector.x, distanceVector.z).normalized;
+            var lookDirection = new Vector2(this.Player.transform.forward.x, this.Player.transform.forward.z).normalized;
 
-            var angle = Vector2.Angle(lookDirection, direction);
+            var angle = AngleBetween(lookDirection, direction) * Mathf.Rad2Deg;
 
             this.Text.text = string.Format("{0:0.00}m", distance);
             this.Image.transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
