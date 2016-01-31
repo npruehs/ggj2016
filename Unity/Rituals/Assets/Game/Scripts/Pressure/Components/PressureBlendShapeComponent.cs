f204697826ac60cf7a0b6771b376884cee69a5cd
Nick Prühs <dev@npruehs.de>
2016-01-31 12:52:59 +0100

    ADDED: Pressure - Blend shape effects.

---
+++ Unity/Rituals/Assets/Game/Scripts/Pressure/Components/PressureBlendShapeComponent.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PressureBlendShapeComponent.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Rituals.Pressure.Components
+{
+    using UnityEngine;
+
+    public class PressureBlendShapeComponent : MonoBehaviour
+    {
+        #region Fields
+
+        public int BlendShape;
+
+        public SkinnedMeshRenderer SkinnedMeshRenderer;
+
+        public float MaximumWeight = 100.0f;
+
+        #endregion
+    }
+}
---
+++ Unity/Rituals/Assets/Game/Scripts/Pressure/Systems/PressureBlendShapeSystem.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PressureBlendShapeSystem.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Rituals.Pressure.Systems
+{
+    using Rituals.Core;
+    using Rituals.Pressure.Components;
+    using Rituals.Pressure.Events;
+
+    public class PressureBlendShapeSystem : RitualsBehaviour
+    {
+        #region Fields
+
+        private PressureBlendShapeComponent[] blendShapes;
+
+        private float currentPressure;
+
+        #endregion
+
+        #region Methods
+
+        protected override void AddListeners()
+        {
+            base.AddListeners();
+
+            this.EventManager.PressureChanged += this.OnPressureChanged;
+        }
+
+        protected override void Init()
+        {
+            base.Init();
+
+            this.blendShapes = FindObjectsOfType<PressureBlendShapeComponent>();
+        }
+
+        protected override void RemoveListeners()
+        {
+            base.RemoveListeners();
+
+            this.EventManager.PressureChanged -= this.OnPressureChanged;
+        }
+
+        private void OnPressureChanged(object sender, PressureChangedEventArgs args)
+        {
+            this.currentPressure = args.Pressure;
+        }
+
+        private void Update()
+        {
+            foreach (var blendShape in this.blendShapes)
+            {
+                blendShape.SkinnedMeshRenderer.SetBlendShapeWeight(
+                    blendShape.BlendShape,
+                    this.currentPressure * blendShape.MaximumWeight);
+            }
+        }
+
+        #endregion
+    }
+}
